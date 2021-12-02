1f9926601b753e31603dd4084bf7807d144fdf16
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2021-12-02 10:32:23 +0100

    refactored code and added config

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
+import requests
 import discord
+import yaml
+
+with open("config.yaml", "r", encoding="utf-8") as fr:
+    config = yaml.load(fr, Loader=yaml.FullLoader)
+
+api_endpoints = {
+    "scoreboard": "https://wiarnvnpsjysgqbwigrn.supabase.co/rest/v1/scoreboard?select=*"
+}
 
 bot = discord.Client()
 
 
 @bot.event
+async def on_ready():
+    print(f"Bot started as {bot.user}")
+
+
+@bot.event
 async def on_message(msg: discord.Message):
-    if msg.content == "!ping":
-        await msg.channel.send("Pong!")
+    if not msg.content.startswith(config["prefix"]):
+        return
+    command_name, *command_args = msg.content[len(config["prefix"]):].split(" ")
+    if command_name == "ping":
+        await command_ping(msg, command_args)
+    elif command_name == "score":
+        await command_score(msg, command_args)
+
+
+async def command_ping(msg: discord.Message, _):
+    await msg.channel.send("Pong!")
+
+
+async def command_score(msg: discord.Message, _):
+    resp = requests.get(api_endpoints["scoreboard"], headers={"apikey": config["api_key"]})
+    if resp.status_code == 200:
+        scoreboard = resp.json()
+
+        await msg.channel.send("\n".join([
+            f'{person["username"]}: {person["score"]} poeng' for person in scoreboard[:10]
+        ]))
+    else:
+        await msg.channel.send("Noe gikk galt!")
+
 
 if __name__ == "__main__":
-    with open("token.txt", "r", encoding="utf-8") as fr:
-        token = fr.read().strip()
-
-    bot.run(token)
+    bot.run(config["bot_key"])
